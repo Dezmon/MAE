30d2e398723825b416a5892266eb905efb14a493
Douglas Perrin <doug2024@gmail.com>
2024-04-22 22:03:57 +0000

    working w/ bad image inport

--- mae_pretrain.py
+++ mae_pretrain.py
@@ -1,10 +1,16 @@
+from ast import arg
 import os
 import argparse
 import math
-import torch
+import torch  # type: ignore
+import torch.cuda
 import torchvision
-from torch.utils.tensorboard import SummaryWriter
-from torchvision.transforms import ToTensor, Compose, Normalize
+from torchvision.io import read_image
+import glob
+from torch.utils.tensorboard.writer import SummaryWriter  
+from torchvision.transforms import Lambda, ToTensor, Compose, Normalize
+from einops import repeat, rearrange
+
 from tqdm import tqdm
 
 from model import *
@@ -13,13 +19,14 @@ from utils import setup_seed
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', type=int, default=42)
-    parser.add_argument('--batch_size', type=int, default=4096)
-    parser.add_argument('--max_device_batch_size', type=int, default=512)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--max_device_batch_size', type=int, default=32)
     parser.add_argument('--base_learning_rate', type=float, default=1.5e-4)
     parser.add_argument('--weight_decay', type=float, default=0.05)
     parser.add_argument('--mask_ratio', type=float, default=0.75)
     parser.add_argument('--total_epoch', type=int, default=2000)
     parser.add_argument('--warmup_epoch', type=int, default=200)
+    parser.add_argument('--data_path', type=str, default='docker-data/torch-test/')
     parser.add_argument('--model_path', type=str, default='vit-t-mae.pt')
 
     args = parser.parse_args()
@@ -32,13 +39,38 @@ if __name__ == '__main__':
     assert batch_size % load_batch_size == 0
     steps_per_update = batch_size // load_batch_size
 
-    train_dataset = torchvision.datasets.CIFAR10('data', train=True, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
-    val_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
-    dataloader = torch.utils.data.DataLoader(train_dataset, load_batch_size, shuffle=True, num_workers=4)
-    writer = SummaryWriter(os.path.join('logs', 'cifar10', 'mae-pretrain'))
+    #train_dataset = torchvision.datasets.CIFAR10('data', train=True, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
+    #val_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
+    
+    class Images(torch.utils.data.Dataset,):  # type: ignore
+        def __init__(self, path: str, transform=None):
+            super().__init__()
+            self.files = [file for file in glob.glob(path+'*.png')]
+            self.transform = transform
+
+        def __getitem__(self, index):
+            #image=read_image(self.files[index])
+            #image=rearrange(torchvision.io.read_image(self.files[index]), 'b h w -> h w b')
+            image=torchvision.io.read_image(self.files[index]).to(torch.float)/255
+            
+            if self.transform:
+                image = self.transform(image)
+            return  image, 'noLabel'
+
+        def __len__(self):
+            return len(self.files)
+        
+    #train_dataset = Images(args.data_path+'train/',   transform=Compose([Lambda(lambda x: x.repeat(1, 1, 3) ), Normalize(0.5, 0.5)]))
+    #val_dataset = Images(args.data_path+'validation/',   transform=Compose([Lambda(lambda x: x.repeat(1, 1, 3) ), Normalize(0.5, 0.5)]))
+    train_dataset = Images(args.data_path+'train/')
+    val_dataset = Images(args.data_path+'validation/')
+    
+    dataloader = torch.utils.data.DataLoader(train_dataset, load_batch_size, shuffle=True, num_workers=4)  # type: ignore
+    
+    writer = SummaryWriter(os.path.join('logs', 'US', 'mae-pretrain'))
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    model = MAE_ViT(mask_ratio=args.mask_ratio).to(device)
+    model = MAE_ViT(image_width=158,image_height=56,patch_height=1,patch_width=158,mask_ratio=args.mask_ratio,image_channels=1).to(device)
     optim = torch.optim.AdamW(model.parameters(), lr=args.base_learning_rate * args.batch_size / 256, betas=(0.9, 0.95), weight_decay=args.weight_decay)
     lr_func = lambda epoch: min((epoch + 1) / (args.warmup_epoch + 1e-8), 0.5 * (math.cos(epoch / args.total_epoch * math.pi) + 1))
     lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optim, lr_lambda=lr_func, verbose=True)
@@ -66,7 +98,7 @@ if __name__ == '__main__':
         ''' visualize the first 16 predicted images on val dataset'''
         model.eval()
         with torch.no_grad():
-            val_img = torch.stack([val_dataset[i][0] for i in range(16)])
+            val_img = torch.stack([val_dataset[i][0] for i in range(4)])
             val_img = val_img.to(device)
             predicted_val_img, mask = model(val_img)
             predicted_val_img = predicted_val_img * mask + val_img * (1 - mask)
--- model.py
+++ model.py
@@ -40,9 +40,11 @@ class PatchShuffle(torch.nn.Module):
 
 class MAE_Encoder(torch.nn.Module):
     def __init__(self,
-                 image_size=32,
-                 image_channels=3,
-                 patch_size=2,
+                 image_width=32,
+                 image_height=32,
+                 image_channels=1,
+                 patch_width=2,
+                 patch_height=2,
                  emb_dim=192,
                  num_layer=12,
                  num_head=4, 
@@ -51,11 +53,11 @@ class MAE_Encoder(torch.nn.Module):
         super().__init__()
 
         self.cls_token = torch.nn.Parameter(torch.zeros(1, 1, emb_dim))
-        self.pos_embedding = torch.nn.Parameter(torch.zeros((image_size // patch_size) ** 2, 1, emb_dim))
+        self.pos_embedding = torch.nn.Parameter(torch.zeros((image_width // patch_width * image_height // patch_height , 1, emb_dim)))
         self.shuffle = PatchShuffle(mask_ratio)
         #three chanels
-        self.patchify = torch.nn.Conv2d(image_channels, emb_dim, patch_size, patch_size)
-
+        #self.patchify = torch.nn.Conv2d(image_channels, emb_dim, patch_width, patch_height)
+        self.patchify =torch.nn.Conv2d(image_channels, emb_dim, (1, patch_width),(1,patch_width))
         self.transformer = torch.nn.Sequential(*[Block(emb_dim, num_head) for _ in range(num_layer)])
 
         self.layer_norm = torch.nn.LayerNorm(emb_dim)
@@ -67,11 +69,12 @@ class MAE_Encoder(torch.nn.Module):
         trunc_normal_(self.pos_embedding, std=.02)
 
     def forward(self, img):
-        patches = self.patchify(img) #convoves by 2x2
-        print('patches dim after conv:', patches.shape)
+        patches = self.patchify(img) 
+        #print('patches dim after conv:', patches.shape)
         patches = rearrange(patches, 'b c h w -> (h w) b c')
+
+        #print('patches dim after rearange:', patches.shape)
         patches = patches + self.pos_embedding
-        print('patches dim after rearange:', patches.shape)
         patches, forward_indexes, backward_indexes = self.shuffle(patches)
         #print('patches dim after shuffle:', patches.shape)
         #does this do anything?
@@ -87,9 +90,11 @@ class MAE_Encoder(torch.nn.Module):
 
 class MAE_Decoder(torch.nn.Module):
     def __init__(self,
-                 image_size=32,
-                 image_channels=3,
-                 patch_size=2,
+                 image_width=32,
+                 image_height=32,
+                 image_channels=1,
+                 patch_width=2,
+                 patch_height=2,
                  emb_dim=192,
                  num_layer=4,
                  num_head=4,
@@ -97,12 +102,12 @@ class MAE_Decoder(torch.nn.Module):
         super().__init__()
 
         self.mask_token = torch.nn.Parameter(torch.zeros(1, 1, emb_dim))
-        self.pos_embedding = torch.nn.Parameter(torch.zeros((image_size // patch_size) ** 2 + 1, 1, emb_dim))
+        self.pos_embedding = torch.nn.Parameter(torch.zeros((image_width // patch_width * image_height // patch_height +1 , 1, emb_dim)))
 
         self.transformer = torch.nn.Sequential(*[Block(emb_dim, num_head) for _ in range(num_layer)])
 
-        self.head = torch.nn.Linear(emb_dim, image_channels * patch_size ** 2)
-        self.patch2img = Rearrange('(h w) b (c p1 p2) -> b c (h p1) (w p2)', p1=patch_size, p2=patch_size, h=image_size//patch_size)
+        self.head = torch.nn.Linear(emb_dim, image_channels * patch_width *patch_height)
+        self.patch2img = Rearrange('(h w) b (c p1 p2) -> b c (h p1) (w p2)', c=image_channels, p2=patch_width, p1=patch_height, w=image_width//patch_width, h=image_height//patch_height)
 
         self.init_weight()
 
@@ -133,9 +138,11 @@ class MAE_Decoder(torch.nn.Module):
 
 class MAE_ViT(torch.nn.Module):
     def __init__(self,
-                 image_size=32,
-                 image_channels=3,
-                 patch_size=2,
+                 image_width,
+                 image_height,
+                 image_channels,
+                 patch_width,
+                 patch_height,
                  emb_dim=192,
                  encoder_layer=12,
                  encoder_head=3,
@@ -144,35 +151,15 @@ class MAE_ViT(torch.nn.Module):
                  mask_ratio=0.75,
                  ) -> None:
         super().__init__()
+                 
 
-        self.encoder = MAE_Encoder(image_size, image_channels, patch_size, emb_dim, encoder_layer, encoder_head, mask_ratio)
-        self.decoder = MAE_Decoder(image_size, image_channels, patch_size, emb_dim, decoder_layer, decoder_head)
+        self.encoder = MAE_Encoder(image_width, image_height, image_channels, patch_width, patch_height, emb_dim, encoder_layer, encoder_head, mask_ratio)
+        self.decoder = MAE_Decoder(image_width, image_height, image_channels, patch_width, patch_height, emb_dim, decoder_layer, decoder_head)
 
     def forward(self, img):
         features, backward_indexes = self.encoder(img)
         predicted_img, mask = self.decoder(features,  backward_indexes)
         return predicted_img, mask
-
-class ViT_Classifier(torch.nn.Module):
-    def __init__(self, encoder : MAE_Encoder, num_classes=10) -> None:
-        super().__init__()
-        self.cls_token = encoder.cls_token
-        self.pos_embedding = encoder.pos_embedding
-        self.patchify = encoder.patchify
-        self.transformer = encoder.transformer
-        self.layer_norm = encoder.layer_norm
-        self.head = torch.nn.Linear(self.pos_embedding.shape[-1], num_classes)
-
-    def forward(self, img):
-        patches = self.patchify(img)
-        patches = rearrange(patches, 'b c h w -> (h w) b c')
-        patches = patches + self.pos_embedding
-        patches = torch.cat([self.cls_token.expand(-1, patches.shape[1], -1), patches], dim=0)
-        patches = rearrange(patches, 't b c -> b t c')
-        features = self.layer_norm(self.transformer(patches))
-        features = rearrange(features, 'b t c -> t b c')
-        logits = self.head(features[0])
-        return logits
 
 
 if __name__ == '__main__':
@@ -184,15 +171,24 @@ if __name__ == '__main__':
     #print('forward index: ',forward_indexes.shape)
 
     #example, chanel(color),w,h
-    img = torch.rand(1, 3, 32, 32)
-    print('img dim:', img.shape)
-    encoder = MAE_Encoder()
-    decoder = MAE_Decoder()
-    features, backward_indexes = encoder(img)
+    img = torch.rand(1, 1, 32, 64)
+    sz=img.shape
+    print('img dim:', sz)
+
+    #patch=torch.nn.Conv2d(image_channels, emb_dim, patch_width, patch_height)(img)
+    #patch=torch.nn.Conv2d(1, 196, 2,2)(img)
+    #print(patch.shape)
+    #patch=torch.nn.Conv2d(1, 196, (1, 64),(1,64))(img)
+    #print(patch.shape)
+    #patch=torch.nn.Conv2d(1, 196, (2, 2),(2,2))(img)
+    #print(patch.shape)
+    encoder = MAE_Encoder( image_width=64,image_height=32,patch_height=1,patch_width=64)
+    decoder = MAE_Decoder( image_width=64,image_height=32,patch_height=1,patch_width=64)
+    #features, backward_indexes = encoder(img)
     #print('backward index: ',backward_indexes.shape)
     #print(backward_indexes)
-    predicted_img, mask = decoder(features, backward_indexes)
+    #predicted_img, mask = decoder(features, backward_indexes)
     #print('mask:', mask.shape)
-    #print(predicted_img.shape)
-    loss = torch.mean((predicted_img - img) ** 2 * mask / 0.75)
+    #print('predicted shape:',predicted_img.shape)
+    #loss = torch.mean((predicted_img - img) ** 2 * mask / 0.75)
     #print(loss)
