89f156cfd38c0797f35283bae4090c57dc9fb41d
Douglas Perrin <doug2024@gmail.com>
2024-05-29 16:22:54 -0400

    working difusion

---
+++ denoising_diffusion.py
@@ -0,0 +1,36 @@
+import os,sys
+sys.path.append('denoising_diffusion_pytorch')
+from denoising_diffusion_pytorch import Unet, GaussianDiffusion, Trainer
+from USUtils.USLoader import *
+
+milestone=0
+print('got in')
+model = Unet(
+    dim = 64,
+    dim_mults = (1, 2, 4,8),
+    flash_attn = False
+)
+
+diffusion = GaussianDiffusion(
+    model,
+    #image_size = (158,56),
+    image_size = (56,160),
+    timesteps = 1000,           # number of steps
+    sampling_timesteps = 250    # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
+)
+trainer = Trainer(
+    diffusion,
+    'docker-data/smalls/',
+    save_and_sample_every =1000,
+    train_batch_size = 16,
+    train_lr = 8e-5,
+    train_num_steps = 700000,         # total training steps
+    gradient_accumulate_every = 2,    # gradient accumulation steps
+    ema_decay = 0.995,                # exponential moving average decay
+    amp = True,                       # turn on mixed precision
+    calculate_fid = False              # whether to calculate fid during training
+)
+print('got in')
+if milestone != 0:
+    trainer.load(milestone)
+trainer.train()
