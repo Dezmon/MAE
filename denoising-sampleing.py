e9215bbda12c86a97842a3b89b616fadf229245f
Douglas Perrin <doug2024@gmail.com>
2024-08-26 08:46:40 -0400

    simplified call for multipul sampling

--- denoising-sampleing.py
+++ denoising-sampleing.py
@@ -36,10 +36,7 @@ diffusion = GaussianDiffusion(
     sampling_timesteps = 250    # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
 )
 
-accelerator = Accelerator(
- #           split_batches = split_batches,
-  #          mixed_precision = mixed_precision_type if amp else 'no'
-        )
+accelerator = Accelerator()
 
 print('starting load')
 data = torch.load(str('results' '/' f'model-{milestone}.pt'), map_location=accelerator.device)
@@ -48,26 +45,21 @@ data = torch.load(str('results' '/' f'model-{milestone}.pt'), map_location=accel
 diffusion.load_state_dict(data['model'])
  
  
- 
 diffusion.step = data['step']
-#diffusion.opt.load_state_dict(data['opt'])
-#if accelerator.is_main_process:
-#    diffusion.ema.load_state_dict(data["ema"])
 
 if 'version' in data:
     print(f"loading from version {data['version']}")
 
-if exists(diffusion.accelerator.scaler) and exists(data['scaler']):
-    diffusion.accelerator.scaler.load_state_dict(data['scaler'])
 
 print('ending load')
 
-
-num_samples = 1
-with accelerator.autocast():   
+num_samples = 2
+shape=(1,3,56,160)
+with accelerator.autocast():  #does this do anything for inference speed or just learing? 
     with torch.inference_mode():
-        all_images_list = list(map(lambda n: diffusion.ddim_sample((1,3,56,160)), range(num_samples)))
+        all_images_list = [diffusion.ddim_sample(shape) for _ in range(num_samples)]
 
 all_images = torch.cat(all_images_list, dim = 0)
 
-utils.save_image(all_images, str('results/' f'sample-new-{milestone}.png'), nrow = int(math.sqrt(num_samples)))
+utils.save_image(all_images, str('results/' f'sample-new-{milestone}.png'), \
+                    nrow = int(math.sqrt(num_samples)))
