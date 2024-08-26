c61628d79a92afefb1991eb8ddb0cb302da05a46
Douglas Perrin <doug2024@gmail.com>
2024-08-26 08:23:26 -0400

    works with class ddim call

--- denoising-sampleing.py
+++ denoising-sampleing.py
@@ -1,11 +1,26 @@
 import os,sys
+import math
 sys.path.append('denoising-diffusion-pytorch')
 from denoising_diffusion_pytorch import Unet, GaussianDiffusion, Trainer # type: ignore
 from USUtils.USLoader import *
-from accelerate import Accelerator
+from accelerate import Accelerator # type: ignore
 from pathlib import Path
+from torchvision import  utils # type: ignore
 
-milestone=1
+
+def num_to_groups(num, divisor):
+    groups = num // divisor
+    remainder = num % divisor
+    arr = [divisor] * groups
+    if remainder > 0:
+        arr.append(remainder)
+    return arr
+
+def exists(x):
+    return x is not None
+
+
+milestone=86
 print('got in')
 model = Unet(
     dim = 64,
@@ -21,134 +36,38 @@ diffusion = GaussianDiffusion(
     sampling_timesteps = 250    # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
 )
 
-class Sampler:
-    def __init__(
-        self,
-        diffusion_model,
-        folder,
-        *,
-        train_batch_size = 16,
-        gradient_accumulate_every = 1,
-        augment_horizontal_flip = True,
-        train_lr = 1e-4,
-        train_num_steps = 100000,
-        ema_update_every = 10,
-        ema_decay = 0.995,
-        adam_betas = (0.9, 0.99),
-        save_and_sample_every = 1000,
-        num_samples = 25,
-        results_folder = './results',
-        amp = False,
-        mixed_precision_type = 'fp16',
-        split_batches = True,
-        convert_image_to = None,
-        calculate_fid = True,
-        inception_block_idx = 2048,
-        max_grad_norm = 1.,
-        num_fid_samples = 50000,
-        save_best_and_latest_only = False
-    ):
-        print('got in') 
-        super().__init__()
-        print('got in')
-        # accelerator
-
-        self.accelerator = Accelerator(
-            split_batches = split_batches,
-            mixed_precision = mixed_precision_type if amp else 'no'
+accelerator = Accelerator(
+ #           split_batches = split_batches,
+  #          mixed_precision = mixed_precision_type if amp else 'no'
         )
 
-        # model
+print('starting load')
+data = torch.load(str('results' '/' f'model-{milestone}.pt'), map_location=accelerator.device)
 
-        self.model = diffusion_model
-        self.channels = diffusion_model.channels
-        is_ddim_sampling = diffusion_model.is_ddim_sampling
+#model = self.accelerator.unwrap_model(self.model)
+diffusion.load_state_dict(data['model'])
+ 
+ 
+ 
+diffusion.step = data['step']
+#diffusion.opt.load_state_dict(data['opt'])
+#if accelerator.is_main_process:
+#    diffusion.ema.load_state_dict(data["ema"])
 
-        # default convert_image_to depending on channels
+if 'version' in data:
+    print(f"loading from version {data['version']}")
 
-        # sampling and training hyperparameters
+if exists(diffusion.accelerator.scaler) and exists(data['scaler']):
+    diffusion.accelerator.scaler.load_state_dict(data['scaler'])
 
-
-      
-
-        # optimizer
-
-        #self.opt = Adam(diffusion_model.parameters(), lr = train_lr, betas = adam_betas)
-
-        # for logging results in a folder periodically
+print('ending load')
 
 
+num_samples = 1
+with accelerator.autocast():   
+    with torch.inference_mode():
+        all_images_list = list(map(lambda n: diffusion.ddim_sample((1,3,56,160)), range(num_samples)))
 
-        self.results_folder = Path(results_folder)
-        self.results_folder.mkdir(exist_ok = True)
+all_images = torch.cat(all_images_list, dim = 0)
 
-        # step counter state
-
-        self.step = 0
-
-        # prepare model, dataloader, optimizer with accelerator
-
-        self.model= self.accelerator.prepare(self.model)
-
-       
-        
-
-    @property
-    def device(self):
-        return self.accelerator.device
-
-    def save(self, milestone):
-        if not self.accelerator.is_local_main_process:
-            return
-
-        data = {
-            'step': self.step,
-            'model': self.accelerator.get_state_dict(self.model),
-            'opt': self.opt.state_dict(),
-            'ema': self.ema.state_dict(),
-            'scaler': self.accelerator.scaler.state_dict() if exists(self.accelerator.scaler) else None,
-            'version': __version__
-        }
-        print('saved in '+ str(self.results_folder / f'model-{milestone}.pt'))
-        torch.save(data, str(self.results_folder / f'model-{milestone}.pt'))
-
-    def load(self, milestone):
-        accelerator = self.accelerator
-        device = accelerator.device
-
-        data = torch.load(str(self.results_folder / f'model-{milestone}.pt'), map_location=device)
-
-        model = self.accelerator.unwrap_model(self.model)
-        model.load_state_dict(data['model'])
-
-        self.step = data['step']
-        #self.opt.load_state_dict(data['opt'])
-        #if self.accelerator.is_main_process:
-        #    self.ema.load_state_dict(data["ema"])
-
-        if 'version' in data:
-            print(f"loading from version {data['version']}")
-
-        #if exists(self.accelerator.scaler) and exists(data['scaler']):
-        #    self.accelerator.scaler.load_state_dict(data['scaler'])
-
-    def sample(self):
-        accelerator = self.accelerator
-        device = accelerator.device
-        print('got in')
-  
-sampler = Sampler(
-    diffusion,
-    '/n/holyscratch01/howe_lab_seas/dperrin/MAE-data/docker-data/fixedsize-torch/train/',
-    save_and_sample_every =1000,
-    train_batch_size = 16,
-    train_lr = 8e-5,
-    train_num_steps = 100000,         # total training steps
-    gradient_accumulate_every = 2,    # gradient accumulation steps
-    ema_decay = 0.995,                # exponential moving average decay
-    amp = True,                       # turn on mixed precision
-    calculate_fid = False              # whether to calculate fid during training
-)
-
-sampler.load(1)
-sampler.sample()
+utils.save_image(all_images, str('results/' f'sample-new-{milestone}.png'), nrow = int(math.sqrt(num_samples)))
